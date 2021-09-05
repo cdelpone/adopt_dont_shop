71a2243d62294977dfb7e03f98f3bf7b17be7028
Christina Delpone <cdelpone@gmail.com>
2021-09-04 20:12:51 -0600

    Test incomplete form redirects to new form page

--- spec/features/applications/new_spec.rb
+++ spec/features/applications/new_spec.rb
@@ -1,13 +1,9 @@
-# Starting an Application
-# visit the pet index page | see a link to "Start an Application" | click link | redirected to the new application page | see a form |  fill in this form with - Name, - Street Address, - City, - State, - Zip Code
-# click submit | redirected to the new application's show page
-# see my Name, address information, and description of why I would make a good home
-# see an indicator that this application is "In Progress"
-
 require 'rails_helper'
 # rspec spec/features/applications/new_spec.rb
 RSpec.describe 'Start Application' do
-  describe 'application form' do
+  describe 'new application form submission' do
+    # Starting an Application
+    # visit the pet index page | see a link to "Start an Application" | click link | redirected to the new application page | see a form |  fill in this form with - Name, - Street Address, - City, - State, - Zip Code | click submit | redirected to the new application's show page | see my Name, address, and description | see an indicator that this application is "In Progress"
     it 'links from pet index' do
       visit '/pets'
 
@@ -21,12 +17,12 @@ RSpec.describe 'Start Application' do
     it 'has a fillable form' do
       visit new_application_path
 
-      fill_in 'Name:', with: 'Melanie'
-      fill_in 'Street Address:', with: '1234 Alphabet Street'
-      fill_in 'City:', with: 'Denver'
-      fill_in 'State:', with: 'Colorado'
-      fill_in 'Zip Code:', with: '86004'
-      fill_in 'Description of why I would make a good home:', with: 'This is why I, Melanie, would make a great home'
+      fill_in 'Name', with: 'Melanie'
+      fill_in 'Street', with: '1234 Alphabet Street'
+      fill_in 'City', with: 'Denver'
+      fill_in 'State', with: 'Colorado'
+      fill_in 'Zip Code', with: '86004'
+      fill_in 'Description of why I would make a good home', with: 'This is why I, Melanie, would make a great home'
 
       expect(page).to have_button("Submit Application")
     end
@@ -34,12 +30,12 @@ RSpec.describe 'Start Application' do
     it 'creates a new form with attributes' do
       visit new_application_path
 
-      fill_in 'Name:', with: 'Melanie'
-      fill_in 'Street Address:', with: '1234 Alphabet Street'
-      fill_in 'City:', with: 'Denver'
-      fill_in 'State:', with: 'Colorado'
-      fill_in 'Zip Code:', with: '86004'
-      fill_in 'Description of why I would make a good home:', with: 'This is why I, Melanie, would make a great home'
+      fill_in 'Name', with: 'Melanie'
+      fill_in 'Street', with: '1234 Alphabet Street'
+      fill_in 'City', with: 'Denver'
+      fill_in 'State', with: 'Colorado'
+      fill_in 'Zip Code', with: '86004'
+      fill_in 'Description of why I would make a good home', with: 'This is why I, Melanie, would make a great home'
 
       expect(page).to have_button("Submit Application")
 
@@ -54,4 +50,29 @@ RSpec.describe 'Start Application' do
       expect(page).to have_content("In Progress")
     end
   end
+
+  describe 'throws error for incomplete forms' do
+    # Starting an Application, Form not Completed
+    # visit the new application page | fail to fill in any of the form fields | click submit | redirected back to the new applications page | see a message that I must fill in those fields.
+    it 'redirects to form when form is incomplete' do
+      visit new_application_path
+
+      fill_in 'Name', with: 'Melanie'
+      fill_in 'Street', with: '1234 Alphabet Street'
+      fill_in 'City', with: 'Denver'
+      fill_in 'Zip Code', with: '86004'
+      fill_in 'Description of why I would make a good home', with: 'This is why I, Melanie, would make a great home'
+
+      click_button "Submit Application"
+
+      expect(current_path).to eq(new_application_path)
+
+      expect(page).to have_content('Incomplete form, please complete required fields')
+    end
+
+    it 'displays an incomplete form error' do
+
+    end
+  end
+
 end
