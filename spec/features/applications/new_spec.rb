3f73ff6ca89ea61a6654ad794c662eb4d7beb788
Christina Delpone <cdelpone@gmail.com>
2021-09-04 19:10:49 -0600

    Test new application page

--- spec/features/applications/new_spec.rb
+++ spec/features/applications/new_spec.rb
@@ -16,15 +16,42 @@ RSpec.describe 'Start Application' do
       click_link 'Start an Application'
 
       expect(current_path).to eq(new_application_path)
-
     end
 
     it 'has a fillable form' do
+      visit new_application_path
 
+      fill_in 'Name:', with: 'Melanie'
+      fill_in 'Street Address:', with: '1234 Alphabet Street'
+      fill_in 'City:', with: 'Denver'
+      fill_in 'State:', with: 'Colorado'
+      fill_in 'Zip Code:', with: '86004'
+      fill_in 'Description of why I would make a good home:', with: 'This is why I, Melanie, would make a great home'
+
+      expect(page).to have_button("Submit Application")
     end
 
     it 'creates a new form with attributes' do
+      visit new_application_path
 
+      fill_in 'Name:', with: 'Melanie'
+      fill_in 'Street Address:', with: '1234 Alphabet Street'
+      fill_in 'City:', with: 'Denver'
+      fill_in 'State:', with: 'Colorado'
+      fill_in 'Zip Code:', with: '86004'
+      fill_in 'Description of why I would make a good home:', with: 'This is why I, Melanie, would make a great home'
+
+      expect(page).to have_button("Submit Application")
+
+      click_button "Submit Application"
+
+      expect(current_path).to eq(application_path(Application.last.id))
+      save_and_open_page
+
+      expect(page).to have_content('Melanie')
+      expect(page).to have_content('1234 Alphabet Street')
+      expect(page).to have_content('This is why I, Melanie, would make a great home')
+      expect(page).to have_content("In Progress")
     end
   end
 end
