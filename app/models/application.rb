66bfc397425525ab6f29111f25b65bb3028812df
Christina Delpone <cdelpone@gmail.com>
2021-09-04 22:02:23 -0600

    Change validation syntax

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,5 +2,9 @@ class Application < ApplicationRecord
   has_many :application_pets
   has_many :pets, through: :application_pets
 
-  validates_presence_of :name, :street, :city, :state, :zip
+  validates :name, presence: true
+  validates :street, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zip, presence: true
 end
