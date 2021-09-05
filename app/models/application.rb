a100c6d8604af263d90122920ae641a269af44c3
Christina Delpone <cdelpone@gmail.com>
2021-09-04 21:18:19 -0600

    Validate presence of application fields

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,6 @@
 class Application < ApplicationRecord
   has_many :application_pets
   has_many :pets, through: :application_pets
+
+  validates_presence_of :name, :street, :city, :state, :zip, :description
 end
