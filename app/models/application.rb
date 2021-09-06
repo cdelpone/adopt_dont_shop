7d8dc89e623421f3a5ced2a5fa75064c8b6ab554
Christina Delpone <cdelpone@gmail.com>
2021-09-06 15:12:16 -0600

    Test validations

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,9 +2,11 @@ class Application < ApplicationRecord
   has_many :application_pets
   has_many :pets, through: :application_pets
 
-  validates :name, presence: true
-  validates :street, presence: true
-  validates :city, presence: true
-  validates :state, presence: true
-  validates :zip, presence: true
+  validates_presence_of :name, :street, :city, :state, :zip
+
+  # validates :name, presence: true
+  # validates :street, presence: true
+  # validates :city, presence: true
+  # validates :state, presence: true
+  # validates :zip, presence: true
 end
