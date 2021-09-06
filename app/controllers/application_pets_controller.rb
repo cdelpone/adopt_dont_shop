d8aa7676efd9d9ebd771a69791cc732b661efe41
Christina Delpone <cdelpone@gmail.com>
2021-09-06 17:16:55 -0600

    Remove comments

--- app/controllers/application_pets_controller.rb
+++ app/controllers/application_pets_controller.rb
@@ -3,7 +3,7 @@ class ApplicationPetsController < ApplicationController
   def create
     application_pets = ApplicationPet.create!(app_pets_params)
     if application_pets.save
-      flash[:success] = "Pet added"
+      flash[:success] = "Pet successfully added to your application!"
       redirect_to application_path(params[:application_id])
     else
       flash[:danger] = "Pet not added for x reason"
