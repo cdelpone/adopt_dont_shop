755b10067ccbd352484656e41d8e67ca59b837de
Christina Delpone <cdelpone@gmail.com>
2021-09-07 15:16:38 -0600

    Add index action to admin shelters controller

--- app/controllers/admin/shelters_controller.rb
+++ app/controllers/admin/shelters_controller.rb
@@ -1,14 +1,6 @@
 class Admin::SheltersController < ApplicationController
 
   def index
-    if params[:sort].present? && params[:sort] == "reverse alpha"
-      @adminshelters = AdminShelter.reverse_order_by_name
-    end
-  end
-
-  private
-
-  def admin_shelter_params
-    params.permit(:name)
+    @shelters = Shelter.reverse_order_by_name
   end
 end
