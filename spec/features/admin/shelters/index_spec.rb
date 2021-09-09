30a228ff485d7dd4c1c4fb22e4b8a57ac7f9d5ee
Christina Delpone <cdelpone@gmail.com>
2021-09-08 19:09:31 -0600

    Test shelters with pending apps section in admin/shelters index

--- spec/features/admin/shelters/index_spec.rb
+++ spec/features/admin/shelters/index_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 # rspec spec/features/admin/shelters/index_spec.rb
 RSpec.describe 'Admin Shelter Index' do
   it 'sorts in reverse alpha' do
-    shelter1 = Shelter.create!(name: 'Mystery Building', city: 'Irvine CA', foster_program: false, rank: 9)
+    shelter1 = Shelter.create(name: 'Mystery Building', city: 'Irvine CA', foster_program: false, rank: 9)
     shelter2 = Shelter.create(name: 'Denver Shelter', city: 'Denver, CO', foster_program: true, rank: 7)
 
     visit '/admin/shelters'
@@ -11,15 +11,28 @@ RSpec.describe 'Admin Shelter Index' do
   end
 
   it 'has a section for shelters with pending applications' do
-    shelter1 = Shelter.create!(name: 'Mystery Building', city: 'Irvine CA', foster_program: false, rank: 9)
+    shelter1 = Shelter.create(name: 'Mystery Building', city: 'Irvine CA', foster_program: false, rank: 9)
+    shelter2 = Shelter.create(name: 'Denver Shelter', city: 'Denver, CO', foster_program: true, rank: 7)
+    shelter3 = Shelter.create(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)
+
     pet1 = shelter1.pets.create(name: 'Scrappy', age: 1, breed: 'Great Dane', adoptable: true)
     pet2 = shelter1.pets.create(name: 'Pesto', age: 5, breed: 'Best Breed', adoptable: true)
+    pet3 = shelter3.pets.create(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true)
+    pet4 = shelter1.pets.create(name: 'Scooby', age: 5, breed: 'Great Dane', adoptable: true)
 
-    shelter2 = Shelter.create(name: 'Denver Shelter', city: 'Denver, CO', foster_program: true, rank: 7)
-    pet4 = shelter2.pets.create(name: 'Scooby', age: 5, breed: 'Great Dane', adoptable: true)
+    application1 = Application.create!(name: 'Xtina', street: '3431 N Vine Street', city: 'Denver', state: 'Colorado', zip: '85523', description: 'this is a description', status: 'Pending')
+    application2 = Application.create!(name: 'Ozzie', street: '3431 N Vine Street', city: 'Denver', state: 'Colorado', zip: '85523', description: 'this is Ozzies description', status: 'In Progress')
+
+    application_pets1 = ApplicationPet.create!(pet_id: pet1.id, application_id: application1.id)
+    application_pets2 = ApplicationPet.create!(pet_id: pet3.id, application_id: application1.id)
 
     visit '/admin/shelters'
-require "pry"; binding.pry
-    expect('Mystery Building').to appear_before('Denver Shelter')
+
+    within("#pending_apps") do
+      expect(page).to have_content("Shelter's with Pending Applications")
+      expect(page).to have_content('Mystery Building')
+      expect(page).to have_content('Fancy pets of Colorado')
+      expect(page).to have_no_content('Denver Shelter')
+    end
   end
 end
