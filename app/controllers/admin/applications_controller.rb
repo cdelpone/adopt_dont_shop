77e9dca28c518b5223a43df1ad11dcc3ba92fdf3
Christina Delpone <cdelpone@gmail.com>
2021-09-08 21:31:22 -0600

    Update app controller

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -6,16 +6,32 @@ class Admin::ApplicationsController < ApplicationController
 
   def update
     application = Application.find(params[:id])
+    require "pry"; binding.pry
+    application_pets = ApplicationPet.find(params[:id])
+    pet = Pet.find(params[:id])
+
     if params[:approve]
-      application.update(application_params.merge({status: "Approved"}))
+      application_pets.update(params.merge({pet_status: "Approved"}))
+      application_pets.update(params.merge({application_status: "Approved"}))
+      # application.update(application_params.merge({status: "Approved"}))
     else params[:reject]
-      application.update(application_params.merge({status: "Rejected"}))
+      application_pets.update(params.merge({pet_status: "Rejected"}))
+      application_pets.update(params.merge({application_status: "Rejected"}))
+      # application.update(application_params.merge({status: "Rejected"}))
     end
       redirect_to admin_application_path(application.id)
   end
 
   private
+  def app_pets_params
+    params.permit(:application_id, :pet_id,)
+  end
+
   def application_params
-    params.permit(:name, :street, :city, :state, :zip, :description, :status)
+    params.permit(:id, :name, :street, :city, :state, :zip, :description, :status)
+  end
+
+  def pet_params
+    params.permit(:id, :name, :age, :breed, :adoptable, :shelter_id)
   end
 end
