c05abb9c8cea53046202deb8585cb11d9f8d8b10
Christina Delpone <cdelpone@gmail.com>
2021-09-08 18:15:32 -0600

    Test approve/reject buttons

--- spec/features/admin/applications/show_spec.rb
+++ spec/features/admin/applications/show_spec.rb
@@ -9,21 +9,27 @@ require 'rails_helper'
 RSpec.describe 'Admin Application Show Page' do
   describe 'Approving a Pet for Adoption' do
     it 'has a button next to each pet to approve' do
-      application1 = Application.create!(name: 'Xtina', street: '3431 N Vine Street', city: 'Denver', state: 'Colorado', zip: '85523', description: 'this is a description', status: 'Pending')
+      shelter = Shelter.create(name: 'Mystery Building', city: 'Irvine CA', foster_program: false, rank: 9)
+      pet = Pet.create(name: 'Scrappy', age: 1, breed: 'Great Dane', adoptable: true, shelter_id: shelter.id)
+      application = Application.create(name: 'Xtina', street: '3431 N Vine Street', city: 'Denver', state: 'Colorado', zip: '85523', description: 'this is a description', status: 'Pending')
+      application_pets = ApplicationPet.create!(pet_id: pet.id, application_id: application.id)
 
-      visit admin_application_path(application1.id)
-      save_and_open_page
+      visit admin_application_path(application.id)
+      # save_and_open_page
       expect(page).to have_button("Approve")
     end
   end
 
-  # describe 'Rejecting a Pet for Adoption' do
-  #   it 'has a button next to each pet to reject' do
-  #     application = Application.create!(name: 'Xtina', street: '3431 N Vine Street', city: 'Denver', state: 'Colorado', zip: '85523', description: 'this is a description', status: 'Pending')
-  #
-  #     visit admin_application_path(application.id)
-  #
-  #     expect(page).to have_button("Reject")
-  #   end
-  # end
+  describe 'Rejecting a Pet for Adoption' do
+    it 'has a button next to each pet to reject' do
+      shelter = Shelter.create(name: 'Mystery Building', city: 'Irvine CA', foster_program: false, rank: 9)
+      pet = Pet.create(name: 'Scrappy', age: 1, breed: 'Great Dane', adoptable: true, shelter_id: shelter.id)
+      application = Application.create(name: 'Xtina', street: '3431 N Vine Street', city: 'Denver', state: 'Colorado', zip: '85523', description: 'this is a description', status: 'Pending')
+      application_pets = ApplicationPet.create!(pet_id: pet.id, application_id: application.id)
+
+      visit admin_application_path(application.id)
+
+      expect(page).to have_button("Reject")
+    end
+  end
 end
