9910b90706b7142e87c32c3466a49e46a9f6b6a3
Christina Delpone <cdelpone@gmail.com>
2021-09-10 10:05:46 -0600

    Test approve button is removed after approving a pet

--- spec/features/admin/applications/show_spec.rb
+++ spec/features/admin/applications/show_spec.rb
@@ -4,17 +4,43 @@ RSpec.describe 'Admin Application Show Page' do
   describe 'Approving a Pet for Adoption' do
     it 'has a button next to each pet to approve' do
       shelter = Shelter.create(name: 'Mystery Building', city: 'Irvine CA', foster_program: false, rank: 9)
-      pet = Pet.create(name: 'Scrappy', age: 1, breed: 'Great Dane', adoptable: true, shelter_id: shelter.id)
+      shelter2 = Shelter.create(name: 'Denver Shelter', city: 'Denver, CO', foster_program: true, rank: 7)
+      pet = shelter.pets.create(name: 'Scrappy', age: 1, breed: 'Great Dane', adoptable: true)
+      # pet = Pet.create(name: 'Scrappy', age: 1, breed: 'Great Dane', adoptable: true, shelter_id: shelter.id)
+      pet2 = shelter.pets.create(name: 'Pesto', age: 5, breed: 'Best Breed', adoptable: true)
+      pet3 = shelter2.pets.create(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true)
+      pet4 = shelter.pets.create(name: 'Scooby', age: 5, breed: 'Great Dane', adoptable: true)
       application = Application.create(name: 'Xtina', street: '3431 N Vine Street', city: 'Denver', state: 'Colorado', zip: '85523', description: 'this is a description', status: 'Pending')
       application_pets = ApplicationPet.create!(pet_id: pet.id, application_id: application.id)
+      application_pets = ApplicationPet.create!(pet_id: pet2.id, application_id: application.id)
 
       visit admin_application_path(application.id)
 
-      expect(page).to have_button("Approve")
+      expect(page).to have_button("Approve Pesto")
+      expect(page).to have_button("Approve Scrappy")
+    end
+
+    it 'redirects to the show page and there is no button to approve the pet' do
+      shelter = Shelter.create(name: 'Mystery Building', city: 'Irvine CA', foster_program: false, rank: 9)
+      pet = shelter.pets.create(name: 'Scrappy', age: 1, breed: 'Great Dane', adoptable: true)
+      pet2 = shelter.pets.create(name: 'Pesto', age: 5, breed: 'Best Breed', adoptable: true)
+      application = Application.create(name: 'Xtina', street: '3431 N Vine Street', city: 'Denver', state: 'Colorado', zip: '85523', description: 'this is a description', status: 'Pending')
+      application_pets = ApplicationPet.create!(pet_id: pet.id, application_id: application.id)
+      application_pets = ApplicationPet.create!(pet_id: pet2.id, application_id: application.id)
+
+      visit admin_application_path(application.id)
+
+      click_button "Approve Pesto"
+
+      expect(current_path).to eq(admin_application_path(application.id))
+
+      application.reload
+
+      expect(page).to have_no_content("Approve Pesto")
     end
   end
 
-  describe 'Rejecting a Pet for Adoption' do
+  xdescribe 'Rejecting a Pet for Adoption' do
     it 'has a button next to each pet to reject' do
       shelter = Shelter.create(name: 'Mystery Building', city: 'Irvine CA', foster_program: false, rank: 9)
       pet = Pet.create(name: 'Scrappy', age: 1, breed: 'Great Dane', adoptable: true, shelter_id: shelter.id)
@@ -23,7 +49,7 @@ RSpec.describe 'Admin Application Show Page' do
 
       visit admin_application_path(application.id)
 
-      expect(page).to have_button("Reject")
+      # expect(page).to have_button("Reject")
     end
   end
 end
