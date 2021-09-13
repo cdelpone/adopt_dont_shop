7c9d3ab181a595cc8a8e25c8f63fbd6627b21c82
Christina Delpone <cdelpone@gmail.com>
2021-09-13 08:37:49 -0600

    Remove app status from joins table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_04_195325) do
+ActiveRecord::Schema.define(version: 2021_09_10_165246) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 2021_09_04_195325) do
     t.bigint "pet_id"
     t.bigint "application_id"
     t.string "pet_status"
-    t.string "application_status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["application_id"], name: "index_application_pets_on_application_id"
